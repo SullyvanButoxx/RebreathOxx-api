c5cfdc6a4e75b3e33e01a2dbac8110445943648c
SullyvanButoxx <butoxxdev@gmail.com>
2018-03-19 18:46:14 +0100

    Create model and test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180319121239) do
+ActiveRecord::Schema.define(version: 20180319174223) do
 
   create_table "rebreathers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", limit: 50
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 20180319121239) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["rebreather_id"], name: "index_sensors_on_rebreather_id"
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "sensors", "rebreathers"
